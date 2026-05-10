7a17d4bf34f2ac05bb8f3f1c77e59a7fba23f4a6
Fairy <125118984+FairyHmm@users.noreply.github.com>
2026-05-10 13:19:18 +0700

    feat: Centers the Canvas

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,8 +1,12 @@
-import { Box } from "@mantine/core";
+import { Center, Box } from "@mantine/core";
 import { useCanvas } from "../hooks/useCanvas";
 
 export default function Canvas(props) {
   const containerRef = useCanvas(props);
 
-  return <Box ref={containerRef} />;
+  return (
+    <Center>
+      <Box ref={containerRef} />
+    </Center>
+  );
 }
