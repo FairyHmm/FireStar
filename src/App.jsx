4eeed648f092cd0d9bb41e6b8eb95b18120e0c9c
Fairy <125118984+FairyHmm@users.noreply.github.com>
2026-05-10 10:25:18 +0700

    refactor: Move theme.css to /src

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { MantineProvider, createTheme } from '@mantine/core';
-import ModeManager from './components/ModeManager';
-import '@mantine/core/styles.css';
-import './styles/theme.css';
+import { MantineProvider, createTheme } from "@mantine/core";
+import ModeManager from "./components/ModeManager";
+import "@mantine/core/styles.css";
+import "./theme.css";
 
 const theme = createTheme({
-  primaryColor: 'violet',
+  primaryColor: "violet",
 });
 
 export default function App() {
   return (
     <MantineProvider theme={theme} defaultColorScheme="dark">
-      <div>
-        <ModeManager />
-      </div>
+      <ModeManager />
     </MantineProvider>
   );
 }
